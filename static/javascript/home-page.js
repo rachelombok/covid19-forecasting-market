905cbf4082ad6f08d77e3516615245828c84f5ce
Aniruddha <aniruddha.murali@gmail.com>
2020-06-29 20:40:40 -0400

    LineChart components in React

--- static/javascript/home-page.js
+++ static/javascript/home-page.js
@@ -1,4 +1,32 @@
-class Navbar extends React.Component{
+function collectData() {
+  var data = null;
+  $.ajax({
+    url: '/forecasts',
+    type: 'get',
+    dataType: 'html',
+    async: false,
+    success: function(response) {
+        data = $.parseJSON(response);
+    } 
+  });
+  
+  var orgs = Object.keys(data);
+  var results = [];
+  for (var i = 0; i < orgs.length; i++) {
+    var forecast = data[orgs[i]];
+    var dates = forecast.target_end_date;
+    var values = forecast.value;
+
+    var result = {};
+    dates.forEach((key, i) => result[key] = values[i]);
+    results.push(result);
+  }
+
+  return results;
+}
+
+
+class Navbar extends React.Component {
     render() {
         return (
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -17,6 +45,45 @@ class Navbar extends React.Component{
     }
 }
 
+
+class LineChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.chartRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.myChart = new Chart(this.chartRef.current, {
+      type: 'line',
+      data: {
+          labels: Object.keys(this.props.data),
+          datasets: [{
+              label: 'Estimated Cases',
+              data: Object.values(this.props.data),
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+    });
+  }
+
+  render() {
+    return <canvas ref={this.chartRef} />;
+  }
+}
+
+
 class App extends React.Component {
   render () {
     return (
@@ -25,79 +92,19 @@ class App extends React.Component {
         <div>
           [Page content here]
         </div>
+        <LineChart data={collectData()[0]} />
+        <LineChart data={collectData()[1]} />
+        <LineChart data={collectData()[2]} />
       </div>
     )
   }
 }
-
-
 
 ReactDOM.render(
     <App />,
     document.getElementById('app')
 );
 
-var forecasts;
 
-$.get("/forecasts", function(data) {
-  forecasts = $.parseJSON(data);
-  console.log(forecasts);
-  collectData(forecasts);
-})
-
-
-function collectData(data) {
-  var orgs = Object.keys(data);
-  for (var i = 0; i < orgs.length; i++) {
-    var forecast = data[orgs[i]];
-    var dates = forecast.target_end_date;
-    var values = forecast.value;
-
-    var result = {};
-    dates.forEach((key, i) => result[key] = values[i]);
-    console.log(result);
-    //console.log(dates[0]);
-    //console.log(values);
-
-    var ctx = document.getElementById('chart' + (i+1).toString()).getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: dates,
-            datasets: [{
-                label: 'Estimated Cases',
-                data: values,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    /*'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    /*'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'*/
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    }); 
-  }
-
-}
 
 
