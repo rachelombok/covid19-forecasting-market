32ca716501e99174f2c9d58970bde536083bef19
Sarah Yoo <gayeon1505@gmail.com>
2020-07-08 23:22:17 +0900

    add LandingPage.js

---
+++ frontend/src/pages/LandingPage/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                <div class="container landing">
+                    <h1 class="logo">COVID-19 Prediction</h1>
+                </div>
+                <div class="landing">
+                    <div class="title"> Welcome<br/>
+                        <button class="button"><b><a href="#">Start</a></b></button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
