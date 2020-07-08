cb4528a73ef7afcc161f7117443cef98da7692c9
Sarah Yoo <gayeon1505@gmail.com>
2020-07-08 23:18:17 +0900

    add routes.js

---
+++ frontend/src/constants/routes.js
@@ -0,0 +1,6 @@
+export const LANDING = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const MAP_PORTAL = '/map-portal';
+export const US_NATIONAL = '/us-national';
+export const US_STATE = '/us-state';
